f5ed6c524459715096622b905c7915b215c4da28
Akash Gupta <70444317+Akashgupta857@users.noreply.github.com>
2026-05-31 16:24:06 +0530

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
 export const resume =
-  "https://drive.google.com/file/d/1wesmMIJgrRQloNwFcRgZMevAfNRAYsZ6/view";
+  "https://drive.google.com/file/d/1qoI8I_mJaj-0V2Lwzw_J-CMPfczKrXh-/view";
 
 export const resumeFileName = "Akash_Gupta_Resume.pdf";
